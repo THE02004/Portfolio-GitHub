aafc310bf6f17433d650b8ecaa34d7e220578844
Théo <141554871+THE02004@users.noreply.github.com>
2024-03-09 19:00:53 +0100

    Add files via upload

--- input.js
+++ input.js
@@ -26,5 +26,6 @@ export class InputHandler {
             }
             console.log(e.key, this.keys);
         })
+        
     }
 }
--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 export class Player {
     constructor(game) {
         this.game = game;
-        this.width = 41;
+        this.width = 32;
         this.height = 52;
         this.x = 0;
         this.y = this.game.height - this.height;
@@ -12,18 +12,26 @@ export class Player {
         this.maxSpeed = 2;
         this.frameX = 0; // Frame horizontale actuelle
         this.frameY = 0;
-        this.previousSpeed = 0; // Stocke la direction précédente du mouvement
+        this.running = false; // Définit si le joueur est en mode de course
+        this.lastDirection = 1; // Stocke la dernière direction du mouvement (1 pour droite, -1 pour gauche)
+        this.frameCount = 0; // Compteur de frame
+        this.maxFrameCount = 5; // Nombre maximal de frames dans l'animation
     }
 
     update(input) {
-        if (input.includes('d') || input.includes('ArrowRight')) {
-            this.speed = this.maxSpeed;
-            this.frameY = 0; // Sélectionner la première ligne de sprites pour la droite
-        } else if (input.includes('q') || input.includes('ArrowLeft')) {
-            this.speed = -this.maxSpeed;
-            this.frameY = 1; // Sélectionner la deuxième ligne de sprites pour la gauche
-        } else {
-            this.speed = 0;
+        if (!this.running) {
+            // Si le joueur n'est pas en mode de course
+            if (input.includes('d') || input.includes('ArrowRight')) {
+                this.speed = this.maxSpeed;
+                this.frameY = 0; // Sélectionner la première ligne de sprites pour la droite
+                this.lastDirection = 1; // Stocke la direction actuelle (droite)
+            } else if (input.includes('q') || input.includes('ArrowLeft')) {
+                this.speed = -this.maxSpeed;
+                this.frameY = 1; // Sélectionner la deuxième ligne de sprites pour la gauche
+                this.lastDirection = -1; // Stocke la direction actuelle (gauche)
+            } else {
+                this.speed = 0;
+            }
         }
 
         if (this.x < 0) this.x = 0; // Limite gauche
@@ -32,29 +40,89 @@ export class Player {
         if (this.y > this.game.height - this.height) this.y = this.game.height - this.height; // Limite inférieure
         this.x += this.speed;
 
-        // Vertical
-        if ((input.includes('ArrowUp') || input.includes('z')) && this.onGround()) this.vy -= 12;
-        this.y += this.vy;
-        if (!this.onGround()) this.vy += this.weight;
-        else this.vy = 0;
-
-        // Mise à jour de la direction précédente du mouvement
+        // Gestion des frames pour l'animation
         if (this.speed !== 0) {
-            this.previousSpeed = this.speed;
+            this.frameCount++;
+            if (this.frameCount >= this.maxFrameCount) {
+                this.frameCount = 0; // Réinitialise le compteur si nécessaire
+            }
+        } else {
+            this.frameCount = 0; // Réinitialise le compteur si le joueur est immobile
+        }
+
+        // Vertical
+        if ((input.includes('ArrowUp') || input.includes('z')) && this.onGround()) {
+            this.vy -= 12;
+        }
+        this.y += this.vy;
+        if (!this.onGround()) {
+            // Si le joueur n'est pas au sol, ajustez la vélocité verticale
+            this.vy += this.weight;
+        } else {
+            // Si le joueur est au sol, réinitialisez la vélocité verticale
+            this.vy = 0;
+            // Assurez-vous que le joueur ne s'enfonce pas dans le sol
+            this.y = this.game.height - this.height;
+        }
+
+        // Activer le mode de course si la touche correspondante est maintenue enfoncée
+        if (this.speed !== 0 && (input.includes('d') || input.includes('ArrowRight') || input.includes('q') || input.includes('ArrowLeft'))) {
+            this.running = true;
+        } else {
+            this.running = false;
         }
     }
 
     draw(context) {
         // Dessiner l'image du joueur en ajustant les coordonnées x et y en fonction de sa position
-        if (this.previousSpeed > 0) {
-            context.drawImage(this.image, 18, 8, this.width, this.height, this.x, this.y, this.width + 10, this.height + 10);
-        } else if (this.previousSpeed < 0) {
-            context.drawImage(this.image, 13, 70, this.width, this.height, this.x, this.y, this.width + 10, this.height + 10);
+        const offsetX = (this.width - this.width) / 2; // Offset en x pour centrer l'image
+        let offsetY = (this.height - this.height) / 2; // Offset en y pour centrer l'image
+        
+        if (this.speed !== 0) {
+            if (this.lastDirection > 0) {
+                this.drawRight(context);
+            } else {
+                this.drawLeft(context);
+            }
         } else {
-            context.drawImage(this.image, 18, 8, this.width, this.height, this.x, this.y, this.width + 10, this.height + 10);
+            if (this.lastDirection > 0) {
+                this.drawStaticRight(context);
+            } else {
+                this.drawStaticLeft(context);
+            }
         }
     }
+    
+    drawRight(context) {
+        const frameY = 0; // Pour la droite
+        const frameX = this.frameCount; // Utilise le compteur de frame pour l'animation
+        const offsetX = (this.width - this.width) / 2; // Offset en x pour centrer l'image
+        let offsetY = (this.height - this.height) / 2; // Offset en y pour centrer l'image
+        context.drawImage(this.image, this.width * frameX + 16, 187, this.width, this.height, this.x - offsetX, this.y - offsetY, this.width, this.height);
+    }
 
+    drawLeft(context) {
+        const frameY = 1; // Pour la gauche
+        const frameX = this.frameCount; // Utilise le compteur de frame pour l'animation
+        const offsetX = (this.width - this.width) / 2; // Offset en x pour centrer l'image
+        let offsetY = (this.height - this.height) / 2; // Offset en y pour centrer l'image
+        context.drawImage(this.image, this.width * frameX + 11, 125  * frameY, this.width, this.height, this.x - offsetX, this.y - offsetY, this.width, this.height);
+    }
+    
+    drawStaticRight(context) {
+        const frameY = 0; // Pour la droite
+        const offsetX = (this.width - this.width) / 2; // Offset en x pour centrer l'image
+        let offsetY = (this.height - this.height) / 2; // Offset en y pour centrer l'image
+        context.drawImage(this.image, this.width * this.frameX + 17, this.height * frameY, this.width, this.height, this.x - offsetX, this.y - offsetY, this.width, this.height);
+    }
+
+    drawStaticLeft(context) {
+        const frameY = 1; // Pour la gauche
+        const offsetX = (this.width - this.width) / 2; // Offset en x pour centrer l'image
+        let offsetY = (this.height - this.height) / 2; // Offset en y pour centrer l'image
+        context.drawImage(this.image, this.width * this.frameX + 10, this.height * frameY + 10, this.width, this.height, this.x - offsetX, this.y - offsetY, this.width, this.height);
+    }
+    
     onGround() {
         return this.y >= this.game.height - this.height;
     }
